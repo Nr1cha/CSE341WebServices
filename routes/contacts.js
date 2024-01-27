ed05b689a399051aac93005326568bdd58861fbb
Nick Richards <vrm.richards@gmail.com>
2024-01-26 21:54:11 -0700

    finishing up routes file for other http routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { getAllContacts } = require("../controllers/contacts");
-const { getSingleContact } = require("../controllers/contacts");
+const {
+    getAllContacts,
+    getSingleContact,
+    updateSingleContact,
+    setSingleContact,
+    deleteSingleContact
+} = require("../controllers/contacts");
+
 
 
 // get the name
 router.get("/", getAllContacts);
 router.get("/:id", getSingleContact);
 
-// router.get('')
+// where to post route
+router.post("/:id", updateSingleContact);
+
+// where to put route
+router.put("/:id", setSingleContact);
+
+// where to delete route
+router.delete("/:id", deleteSingleContact);
 
 module.exports = router;
